f9c1cf7a63a96ee7150068ce84de9dfa7dd6a4d6
gsicvj <da.mc.duc@gmail.com>
2023-04-03 15:07:28 +0200

    Init project

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
-import { TinaMarkdown } from "tinacms/dist/rich-text";
-import { Layout } from "../components/Layout";
-import { useTina } from "tinacms/dist/react";
-import { client } from "../tina/__generated__/client";
+import React from "react";
 
 export default function Home(props) {
-  // data passes though in production mode and data is updated to the sidebar data in edit-mode
-  const { data } = useTina({
-    query: props.query,
-    variables: props.variables,
-    data: props.data,
-  });
-
-  const content = data.page.body;
   return (
-    <Layout>
-      <TinaMarkdown content={content} />
-    </Layout>
+    <h1>Index page loaded</h1>
   );
 }
-
-export const getStaticProps = async () => {
-  const { data, query, variables } = await client.queries.page({
-    relativePath: "home.mdx",
-  });
-
-  return {
-    props: {
-      data,
-      query,
-      variables,
-      //myOtherProp: 'some-other-data',
-    },
-  };
-};
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,52 +1,9 @@
-import { Layout } from "../../components/Layout";
-import { useTina } from "tinacms/dist/react";
-import { client } from "../../tina/__generated__/client";
+import React from "react";
 
 export default function Home(props) {
-  // data passes though in production mode and data is updated to the sidebar data in edit-mode
-  const { data } = useTina({
-    query: props.query,
-    variables: props.variables,
-    data: props.data,
-  });
-
   return (
-    <Layout>
-      <code>
-        <pre
-          style={{
-            backgroundColor: "lightgray",
-          }}
-        >
-          {JSON.stringify(data.post, null, 2)}
-        </pre>
-      </code>
-    </Layout>
+    <div className="no-layout">
+      <h1>Slug loaded successfully.</h1>
+    </div>
   );
 }
-
-export const getStaticPaths = async () => {
-  const { data } = await client.queries.postConnection();
-  const paths = data.postConnection.edges.map((x) => {
-    return { params: { slug: x.node._sys.filename } };
-  });
-
-  return {
-    paths,
-    fallback: "blocking",
-  };
-};
-
-export const getStaticProps = async (ctx) => {
-  const { data, query, variables } = await client.queries.post({
-    relativePath: ctx.params.slug + ".md",
-  });
-
-  return {
-    props: {
-      data,
-      query,
-      variables,
-    },
-  };
-};
